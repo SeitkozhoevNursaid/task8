482768d6f4d209a5999873d2f6d7c2254f8c88c7
unknown <nursaid.seitkozhoev@mail.ru>
2024-04-24 17:37:44 +0600

    user.urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from user.views import (
+    RegistrationAPIView,
+    VerifyEmail,
+    ChangePassword,
+    ForgotPassword,
+    ConfirmForgotPassword,
+    AdminResetPassword,
+)
 
 
 urlpatterns = [
     path('auth/', TokenObtainPairView.as_view()),
     path('auth/refresh', TokenRefreshView.as_view()),
+    path('registration/', RegistrationAPIView.as_view()),
+    path('email-verify/<uuid:activation_code>/', VerifyEmail.as_view()),
+    path('change_password/', ChangePassword.as_view()),
+    path('forgot-password/', ForgotPassword.as_view()),
+    path('confirm-forgot-password/<str:pk>/', ConfirmForgotPassword.as_view()),
+    path('reset_password/', AdminResetPassword.as_view())
 ]
