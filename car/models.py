200eb4bcd6f8dcf22d88fd1101281f3e3fcd3f8d
unknown <nursaid.seitkozhoev@mail.ru>
2024-04-26 10:59:23 +0600

    configuration images

--- car/models.py
+++ car/models.py
@@ -13,7 +13,7 @@ class Car(models.Model):
     name = models.CharField(("Название машины"), max_length=50)
     description = models.CharField(("Описание машины"), max_length=250)
     price = models.IntegerField(('Цена машины'))
-    images = models.ImageField(("Фото машины"), upload_to=None)
+    images = models.ImageField(("Фото машины"), upload_to='images/', null=True, blank=True)
     category = models.ForeignKey(Category, verbose_name=('Категория'), on_delete=models.CASCADE,)
 
     class Meta:
