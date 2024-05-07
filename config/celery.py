0dd7109d72f79002c85604eca489410d5f6c46e3
unknown <nursaid.seitkozhoev@mail.ru>
2024-05-07 09:54:31 +0600

    correct

--- config/celery.py
+++ config/celery.py
@@ -6,7 +6,3 @@ app = Celery("config")
 app.conf.broker_connection_retry_on_startup = True
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-
-@app.task
-def add(x, y):
-    return x + y
